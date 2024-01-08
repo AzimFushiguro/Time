913e9d3b8cf61c725680e5443fb42ec2dbd4820f
Azim <FushiguroAzim@gmail.com>
2024-01-08 13:59:07 +0500

    one commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Calendar;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date.getTime());
+        Calendar calendar = Calendar.getInstance();
+        System.out.println(calendar.getTime());
+        System.out.println(calendar.getTimeZone());
+        System.out.println(calendar.get(Calendar.HOUR));
+        System.out.println(calendar.get(Calendar.MINUTE));
+        System.out.println(calendar.get(Calendar.MILLISECOND));
+    }
+}
